9d0524ad9e3eaeb0e3f24f38768d90acc5b7b0a7
kjayawant <karishmajayawant@gmail.com>
2019-04-10 22:52:30 -0700

    Update nthprime.cpp

--- nthprime.cpp
+++ nthprime.cpp
@@ -1,36 +1,3 @@
-vector<int> spiralMatrix(vector <vector <int>> &input_mat) {
-	int x = input_mat.size();
-	int y = input_mat[0].size();
-
-	int up = 0, down = x - 1, left = 0, right = y - 1;
-	int k = 0;
-	vector <int> res(x*y);
-	while (true) {
-		for (int i = left; i <= right; i++) {
-			res[k++] = input_mat[up][i];
-		}
-		up++;
-		if (up>down) break;
-
-		for (int j = up; j <= down; j++) {
-			res[k++] = input_mat[j][right];
-		}
-		right--;
-		if (right<left) break;
-		for (int i = right; i >= left; i--) {
-			res[k++] = input_mat[down][i];
-		}
-		down--;
-		if (down<up) break;
-		for (int j = down; j >= up; j--) {
-			res[k++] = input_mat[j][left];
-		}
-		left++;
-		if (left>right) break;
-	}
-
-	return res;
-}
 int getnthprime() {
 	int count = 2;
 	int check = 6;
